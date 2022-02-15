c6b69f9233ab9d8e2f015db5701b3476db241fb2
berto <berto@LAPTOP-HHMEGA06>
2022-02-14 23:04:35 -0600

    Login

---
+++ src/main/java/ventana/Principal.java
@@ -0,0 +1,11 @@
+
+package ventana;
+
+public class Principal {
+  
+    public static void main(String[] args) {
+        Inicio objInicioSecion = new Inicio();
+        objInicioSecion.setVisible(true);
+    }
+  
+}
